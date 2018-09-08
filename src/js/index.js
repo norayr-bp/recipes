cbcbd235424469b0211e820efee3c2906a48199b
Norayr <norayr.ghukasian@gmail.com>
2018-09-08 11:43:41 +0400

    add recipe view with its ingredients into UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global state of the map
@@ -68,18 +69,29 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
-        state.recipe = new Recipe(id);
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlight selected search item
+        if (state.search) {
+            searchView.highlightSelected(id);
+        }
+        
         // Create new recipe object
+        state.recipe = new Recipe(id);
         try {
             // Get recipe data
             await state.recipe.getRecipe();
+            console.log(state.recipe, 'sdjfghsfgjsgf');
+            state.recipe.parseIngredients();
             // Calculate servings and time
             state.recipe.calcTime();
             state.recipe.calcServings();
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch(error) {
-            alert('error processing recipe!');
+            console.log(error, 'error processing recipe!');
         }
     }
 }
@@ -88,3 +100,17 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+});
