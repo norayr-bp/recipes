82b45659c0318f54a9c1b33f5716938589253171
Norayr <norayr.ghukasian@gmail.com>
2018-09-08 22:26:39 +0400

    change the key for API calls

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,4 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = 'ed286e8d0fa411cd4df6f96cc315ce0a';
+export const key = '462b1cc8d4f2730081462fbc65136320';
+// key 462b1cc8d4f2730081462fbc65136320
+// key ed286e8d0fa411cd4df6f96cc315ce0a
